0ef0d588eacad4eb0fc11ccf4ae4dccd7f00ade1
mpate133 <mpate133@asu.edu>
2022-10-31 09:52:34 -0700

    first commit

---
+++ hacs/AssignmentMenu.java
@@ -0,0 +1,22 @@
+package hacs;
+
+import javax.swing.*;
+
+/**
+ * Title: HACS
+ * Description:
+ * Copyright: Copyright (c) 2002
+ * Company: msu
+ *
+ * @author Zhang ji Zhu Wei
+ * @version 1.0
+ */
+
+abstract public class AssignmentMenu extends JDialog {
+    public AssignmentMenu() {
+        setModal(true);
+        setSize(575, 330);
+    }
+
+    abstract void ShowMenu(Assignment ass, Person per);
+}
---
+++ hacs/SolutionMenu.java
@@ -0,0 +1,24 @@
+package hacs;
+
+import javax.swing.*;
+
+/**
+ * Title: HACS Description: Copyright: Copyright (c) 2002 Company: msu
+ *
+ * @author Zhang ji Zhu Wei
+ * @author mjfindler
+ * @version 2.0
+ * <p>
+ * Update to Java 8
+ */
+
+public class SolutionMenu extends JDialog {
+
+    public SolutionMenu() {
+    }
+
+    void ShowMenu(Solution theSolution) {
+        show();
+    }
+
+}
