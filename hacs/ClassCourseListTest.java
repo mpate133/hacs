b54bc216baba91512e43af4b285347d9062e1031
mpate133 <mpate133@asu.edu>
2022-11-03 14:11:05 -0700

    committing test case files

--- hacs/ClassCourseListTest.java
+++ hacs/ClassCourseListTest.java
@@ -1,12 +1,15 @@
 package hacs;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ClassCourseListTest {
+
+    ClassCourseList classCourseList = new ClassCourseList();
 
     @BeforeEach
     void setUp() {
@@ -18,9 +21,14 @@ class ClassCourseListTest {
 
     @Test
     void initializeFromFile() {
+        classCourseList.initializeFromFile("./TextFiles/CourseInfo.txt");
+        Assertions.assertEquals(3,classCourseList.size());
     }
 
     @Test
     void findCourseByCourseName() {
+        String course = "SER501";
+        classCourseList.initializeFromFile("./TextFiles/CourseInfo.txt");
+        assertEquals(course, classCourseList.findCourseByCourseName((course)).toString());
     }
 }
--- hacs/CourseIteratorTest.java
+++ hacs/CourseIteratorTest.java
@@ -1,24 +1,40 @@
 package hacs;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CourseIteratorTest {
 
+    Course course = new Course("SER501", 1);
+    ClassCourseList classCourseList = new ClassCourseList();
+    CourseIterator iter = new CourseIterator(classCourseList);
+
+
     @Test
     void remove() {
+        classCourseList.add(course);
+        iter.next();
+        iter.remove();
+        Assertions.assertEquals(0, iter.theCourseList.size());
     }
 
     @Test
     void hasNext() {
+        classCourseList.add(course);
+        Assertions.assertEquals(true, iter.hasNext());
     }
 
     @Test
     void next() {
+        classCourseList.add(course);
+        Assertions.assertNotEquals(null, iter.next());
     }
 
     @Test
     void testNext() {
+        classCourseList.add(course);
+        Assertions.assertNotEquals(null, iter.next("SER501"));
     }
 }
