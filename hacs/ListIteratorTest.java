53891aa8201a7a29dbbc63fabf4603e7d28a5ad2
mpate133 <mpate133@asu.edu>
2022-11-03 15:27:38 -0700

    regular commit

--- hacs/ListIteratorTest.java
+++ hacs/ListIteratorTest.java
@@ -1,12 +1,20 @@
 package hacs;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.ListIterator;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ListIteratorTest {
+
+    ArrayList<Object> arrList = new ArrayList<>();
+    hacs.ListIterator iter = new hacs.ListIterator(arrList);
 
     @BeforeEach
     void setUp() {
@@ -18,13 +26,30 @@ class ListIteratorTest {
 
     @Test
     void remove() {
+        arrList.add("first addition");
+        arrList.add("second addition");
+        iter.next();
+        iter.remove();
+        Assertions.assertEquals(1, iter.theList.size());
+        iter.next();
+        iter.remove();
+        Assertions.assertEquals(0, iter.theList.size());
+
     }
 
     @Test
     void hasNext() {
+        arrList.add("first");
+        Assertions.assertEquals(true, iter.hasNext());
+        iter.next();
+        iter.remove();
+        Assertions.assertEquals(false, iter.hasNext());
     }
 
     @Test
     void next() {
+        Assertions.assertEquals(null, iter.next());
+        arrList.add("first");
+        Assertions.assertEquals("first", iter.next());
     }
 }
